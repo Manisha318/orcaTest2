2f6a535afc813c32227245d1d4e7de1959bffcdb
Manisha318 <155012587+Manisha318@users.noreply.github.com>
2023-12-29 17:07:09 +0530

    Add files via upload

---
+++ seed.js
@@ -0,0 +1,22 @@
+const vendorSeed = [
+    {
+        vendorName: "jeep",
+        password: "jeep",
+        email: "jeep@gmail.com",
+        standoutFeatures: ["phone app", "email notifications"]
+    },
+    {
+        vendorName: "tata",
+        password: "tata",
+        email: "tata@gmail.com",
+        standoutFeatures: ["phone app", "email notifications"]
+    },
+    {
+        vendorName: "bmw",
+        password: "bmw",
+        email: "bmw@gmail.com",
+        standoutFeatures: ["phone app", "email notifications"]
+    }
+]
+
+export {vendorSeed}
